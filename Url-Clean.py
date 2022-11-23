793029dbacbbcc6378fe25e73301c43581865cd3
インストール <54149263+DevilXcry@users.noreply.github.com>
2022-11-23 18:35:00 +0600

    Some changes

--- Url-Clean.py
+++ Url-Clean.py
@@ -1,8 +1,11 @@
 lastDomain = []
 print("\n......Working")
 url = open('url.txt', "r", errors='ignore').readlines()
+total = len(url)
+v = []
+print(f"Total Link: {total}")
 for i in url:
-    if '?' and '=' in i and i.startswith('h'):
+    if '?' and '=' in i and i.startswith('h') and ".kr" not in i:
         try:
             currentDomain = i.split('/')[2].strip()
 
@@ -12,10 +15,13 @@ for i in url:
               lastDomain.append(currentDomain)
               save = open("valid.txt", 'a')
               save.write(f"{i}")
+              v.append("0")
         except:
             continue
     
     else:
         continue  
 
+total_v = len(v)
+print(f"valid link:{total_v}")
 input("Finished......")
